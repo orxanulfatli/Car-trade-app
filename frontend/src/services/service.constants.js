f7f0968df8abcc1308b8c215ba833852fc5af91d
Orxan Ulfatli <orxanulfatli@gmail.com>
2022-10-12 20:53:34 +0400

    api route

--- frontend/src/services/service.constants.js
+++ frontend/src/services/service.constants.js
@@ -1,9 +1,10 @@
 
 
-const devEnv = process.env.NODE_ENV !== 'production'
-const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
+// const devEnv = process.env.NODE_ENV !== 'production'
+// const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
 
-export const API_URL = devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API;
+// export const API_URL = devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API;
+export const API_URL ='http://localhost:5000/api/v1/'||process.env.PORT;
 export const LOGIN_URL = "signin";
 export const VERIFY_URL = "verify";
 export const REFRESH_URL = "refresh";
